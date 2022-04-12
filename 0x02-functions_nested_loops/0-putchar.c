6d50b33a0e104e9f7263e0cdf888c1770f40996f
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-12 17:31:32 +0100

    c programs

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - main block
  * Return:0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
+
 }
