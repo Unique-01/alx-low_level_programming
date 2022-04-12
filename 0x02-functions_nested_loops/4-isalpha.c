88253adfbd16d0829b43b3bf0562b5d8c43db226
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-12 20:40:56 +0100

    c programs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - check the code
+ * @c: int
  * Return: 1(success) 0(failure)
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c<= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
