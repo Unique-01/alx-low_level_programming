b0772212917cbbf5a2ce0a0399553dfc5df708a9
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-13 08:12:50 +0100

    c programs

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - check the code
+ * @n: int
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int num = 0;
+
+	if (n > 98)
+	{
+		for (num = n; num >= 98; num--)
+		{
+			if (num != n)
+			{
+				printf(",");
+			}
+			printf("%d", num);
+		}
+	}
+	else
+	{
+		for (num = n; num <= 98; num++)
+		{
+			if (num != n)
+			{
+				printf(",");
+			}
+			printf("%d", num);
+		}
+	}
+	putchar('\n');
+}
