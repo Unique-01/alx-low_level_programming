243a8b0bcbd10fc3693fd82425183e90ee437bb2
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-15 10:01:56 +0100

    c programs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print number 0 - 9
+ * Return: 0
+ *
+ */
+
+void print_numbers(void)
+{
+    int c;
+
+    for (c = 0; c < 10; c++)
+    {
+        _putchar(c + '0');
+    }
+    _putchar('\n');
+}
