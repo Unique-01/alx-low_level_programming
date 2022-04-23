f38e1cea0bb457610aa0fd9f2bfe854872ecc0c0
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-23 10:10:44 +0100

    c pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,15 @@
 
 void print_rev(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        i++;
-    }
-    while (i--)
-    {
-        putchar(s[i]);
-    }
-    putchar('\n');
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
 }
