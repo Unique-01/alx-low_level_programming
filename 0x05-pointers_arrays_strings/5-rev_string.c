f017fb6b46982b3fab566170aa3c198831de756b
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-23 10:30:31 +0100

    c pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,20 +10,22 @@
 
 void rev_string(char *s)
 {
-	int i = 0, j;
-	char temp;
+	int i = 0, j, right;
 
 	while (s[i])
-	{
 		i++;
-	}
 
 	j = i / 2;
+	right = 0;
 
-	while (j--)
+	while (right != j)
 	{
-		temp = putchar(s[i]);
-	}
+		char temp = s[right];
+		int left = i - right - 1;
 
-	*s = temp;
+		s[right] = s[left];
+		s[left] = temp;
+
+		right++;
+	}
 }
