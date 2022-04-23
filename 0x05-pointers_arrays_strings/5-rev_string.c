9c641fec6f71316adfad2ddfcef5e11902879e0f
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-23 10:18:57 +0100

    c pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse a string
+ * @s: the string to be reverse
+ * Return: Void
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char temp;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		temp = putchar(s[i]);
+	}
+
+	*s = temp;
+}
