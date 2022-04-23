96dafa0cc4d33cb08df7ab1f2a155ed79faaed36
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-23 10:09:47 +0100

    c pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev -  print a string in reverse order
+ * @s: the string to be reverse
+ * Return: Void
+ *
+ */
+
+void print_rev(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        i++;
+    }
+    while (i--)
+    {
+        putchar(s[i]);
+    }
+    putchar('\n');
+}
