bbded39820bbb584b5c78e5e3953647df01d6a98
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-23 10:02:39 +0100

    c pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - print a string to stdout
+ * @str: input string
+ * Return: Void
+ *
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
