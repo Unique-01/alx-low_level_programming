fdb6e56499869ea7b5ca927577210584828ef5eb
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-28 18:39:05 +0100

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * is_palindrome - return 1 if string is palindrome and 0 otherwise
+ * @s: the string to check
+ * Return: 0 of 1
+ *
+ */
+
+int is_palindrome(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
