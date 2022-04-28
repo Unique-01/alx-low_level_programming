8c4978ffc1be4f994dd6bace43da19fd1fbdd335
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-04-28 17:42:49 +0100

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,17 +3,16 @@
  * _print_rev_recursion - Print the reverse of a string
  * @s: Pointer to the string
  * Return: void
- * 
+ *
  */
 
 void _print_rev_recursion(char *s)
 {
-    if (s[0] == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(s[0]);
-    _putchar(s - 1);
-    
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_print_rev_recursion(s - 1);
 }
