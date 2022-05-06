f0b1a2971c78ef5bbe3f032dd3e92f7bec7c391d
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-05-06 13:34:25 +0100

    c argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,28 @@
  */
 int main(int argc, char *argv[])
 {
-    unsigned int i, sum, num;
+	unsigned int i, sum, num;
 
-    sum = 0;
+	sum = 0;
 
-    if (argc < 3)
-    {
-        printf("%d\n", 0);
-        return (0);
-    }
-    while (argc-- && argc > 0)
-    {
-        for (i = 0; argv[argc][i] != '\0'; i++)
-        {
-            if (!(isdigit(argv[argc][i])))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        num = atoi(argv[argc]);
-        sum += num;
-    }
-    printf("%d\n", sum);
-    return (sum);
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
 }
