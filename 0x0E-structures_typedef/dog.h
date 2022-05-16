f1d136d124328d712d193a54281e43ba409cd086
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-05-16 21:28:10 +0100

    c structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Describing of a dog
+ * @name:  The name of the dog
+ * @age: Thee age of the dog
+ * @owner: The owner of the dog
+ *
+ */
 struct dog
 {
-    char *name;
-    float *age;
-    char *owner;
+	char *name;
+	float *age;
+	char *owner;
 };
 
 #endif
