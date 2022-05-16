fa0c7ffd8693a816f4aa10816dd0e76e2c46dada
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-05-16 17:57:32 +0100

    c variadic funcs

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,30 +1,31 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_numbers - print the number passed in as arguments with the separator
- * @separator: To separate the Numbers
- * @n:number of parameters
- * @...: variable numbers to be printed
- * Return: the numbers
+ * print_numbers - prints numbers
+ * @separator: pointer to the string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	int i;
+	unsigned int i;
+	va_list params;
 
-	va_start(args, n);
+	va_start(params, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1) && n != 0; i++)
 	{
-		_putchar(va_arg(args, int));
-		if (i != (n - 1) && separator != NULL)
-		{
-			_putchar(separator);
-		}
+		if (!separator)
+			printf("%d", va_arg(params, int));
+		else
+			printf("%d%s", va_arg(params, int), separator);
 	}
-	_putchar("\n");
-	va_end(args);
+
+	if (n)
+		printf("%d\n", va_arg(params, int));
+	else
+		printf("\n");
+
+	va_end(params);
 }
